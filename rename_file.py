c3486742231f073e54610381c1fd0a6aeeac609a
anarghya15 <anarghya.gayathri@gmail.com>
2024-05-01 00:35:42 +0530

    Add logic to count files

--- rename_file.py
+++ rename_file.py
@@ -1,10 +1,15 @@
 import os
 
 # Main directory containing subfolders
-main_directory = r"D:\\IIIT\\AI705 - RS\\City Images"
+#main_directory = r"D:\\IIIT\\AI705 - RS\\City Images"
+main_directory = r"/media/dell/New Volume/IIIT/AI705 - RS/City Images"
+
 
 # Iterate over each subfolder in the main directory
 for root, dirs, files in os.walk(main_directory):
+    num_files = len(files)
+    if num_files < 5:
+        print(f"Folder '{root}' has less than 5 files: {num_files} files")
     for file in files:
         file_path = os.path.join(root, file)
         if '&' in file:
